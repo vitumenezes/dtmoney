8996b004467f6fdd3ec4bf35cf679100bb24d601
Victor Menezes <victormenezez15@gmail.com>
2021-04-16 01:44:09 -0300

    Form structure. Modal styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Modal from 'react-modal';
 
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
-import { NewTransactionModal } from './NewTransactionModal';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from './styles/global';
 
