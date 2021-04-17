82949aeaef7bbcc717fea096a87cca192c82c568
Victor Menezes <victormenezez15@gmail.com>
2021-04-17 03:46:36 -0300

    Summary calc

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -22,15 +22,22 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     const [amount, setAmount] = useState(0);
     const [category, setCategory] = useState('');
 
-    function handleCreateNewTransaction(event: FormEvent) {
+    async function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
     
-        createTransaction({
+        await createTransaction({
             title,
             amount,
             category,
             type,
-        })
+        });
+
+        setTitle('');
+        setAmount(0);
+        setType('deposit');
+        setCategory('');
+
+        onRequestClose();
     }
     
     return (
